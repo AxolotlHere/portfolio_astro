4e034a58d52f2bccd48ce51bfcf2d7649c6d4f1f
AxolotlHere <snaveenbharath2005@gmail.com>
2025-06-19 13:11:53 +0530

    Holy projects template

---
+++ src/components/bleedCarousel.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import Carousel from "../components/ui/carousel";
+export function CarouselDemo() {
+  const slideData = [
+    {
+      title: "Mystic Mountains",
+      button: "Explore Component",
+      desc: "cool asf project if you ask me",
+    },
+    {
+      title: "Urban Dreams",
+      button: "Explore Component",
+      desc: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Neon Nights",
+      button: "Explore Component",
+      desc: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      title: "Desert Whispers",
+      button: "Explore Component",
+      desc: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+  ];
+  return (
+    <div className="relative overflow-hidden w-full h-full py-1">
+      <Carousel slides={slideData} />
+    </div>
+  );
+}
+
